94991c343f0b3c3742bf839a4057dc9933b46503
ilhamSkyOf19 <ilhamm778866@gmail.com>
2025-09-04 14:29:48 +0700

    update welcome

--- client/src/pages/Welcome/index.tsx
+++ client/src/pages/Welcome/index.tsx
@@ -2,8 +2,8 @@ import { type FC } from 'react'
 
 const Welcome: FC = () => {
     return (
-        <div className='w-full flex flex-row justify-center items-center h-[100vh] pt-12 z-10'>
-            <h1 className='font-bold text-white'>Welcome asdsa</h1>
+        <div className='w-full flex flex-row justify-center items-end h-[50vh] pt-12 z-10'>
+            <h1 className='font-bold text-white text-5xl text-center leading-[4rem]'>Welcome aboard, let’s <br /> get started on your journey.</h1>
         </div>
     )
 }
