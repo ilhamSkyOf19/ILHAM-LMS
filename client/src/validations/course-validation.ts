1bfd804825deff69d23fe49683653a7dfc3a5120
ilhamSkyOf19 <ilhamm778866@gmail.com>
2025-09-12 17:07:04 +0700

    create course validation

---
+++ client/src/validations/course-validation.ts
@@ -0,0 +1,12 @@
+// import z from "zod";
+// import type { CreateCourseModel } from "../models/course-model";
+
+
+// export class CourseValidation {
+
+//     // create 
+//     static readonly CREATE = z.object({
+//         name: z.string().min(1, 'name is required'),
+        
+//     }).strict() satisfies z.ZodType<CreateCourseModel>
+// }
